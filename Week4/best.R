6e561c2383890555d8e70f7e4dea506a29f906a1
Christopher Hair <mystikweb@live.ca>
2017-04-05 22:28:15 -0400

    First function working but getting warnings in numeric conversions

--- Week4/best.R
+++ Week4/best.R
@@ -14,8 +14,23 @@ best <- function(state, outcome) {
     }
 
     # Get which outcome is being selected
-    selectedOutcome <- possibleOutcomes[outcome]
-
+    outcomeLocation <- match(outcome, possibleOutcomes)
+    
     # Validate that the outcome is one of the possible outcomes
-    print(selectedOutcome)
+    if (is.na(outcomeLocation)) {
+        stop("invalid outcome")
+    }
+
+    selectedOutcome <- possibleOutcomes[outcomeLocation]
+
+    if (selectedOutcome == "heart attack") {
+        lowest <- stateValues[which(as.numeric(stateValues$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack) == min(as.numeric(stateValues$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack), na.rm = TRUE)),]
+    } else if (selectedOutcome == "heart failure") {
+        lowest <- stateValues[which(as.numeric(stateValues$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure) == min(as.numeric(stateValues$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), na.rm = TRUE)),]
+    } else {
+        lowest <- stateValues[which(as.numeric(stateValues$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia) == min(as.numeric(stateValues$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia), na.rm = TRUE)),]
+    }
+
+    ordered <- lowest[order(lowest$Hospital.Name),]
+    print(ordered[1,]$Hospital.Name)
 }
